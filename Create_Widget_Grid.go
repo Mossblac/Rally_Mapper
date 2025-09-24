b1d8f2c1ab5d8445d3cda2b570c983ffc3c35608
Mossblac <Murphyrmurphy2000@gmail.com>
2025-09-24 10:36:03 -0600

    setImageInCell updated

--- Create_Widget_Grid.go
+++ Create_Widget_Grid.go
@@ -11,17 +11,17 @@ import (
 	"fyne.io/fyne/v2/widget"
 )
 
-func Grid_Widget(value string, numObstacles int) {
+func Grid_Widget(TrackType string, numObstacles int) {
 	var numCols int
 	var numRows int
 	twiceObSize := float32(numObstacles * 2)
 
-	if value == "loop" {
+	if TrackType == "loop" {
 		numCols = 3 * numObstacles
 		numRows = 3 * numObstacles
 	}
 
-	if value == "linear" {
+	if TrackType == "linear" {
 		numCols = 3
 		numRows = 3 * numObstacles
 	}
@@ -33,14 +33,14 @@ func Grid_Widget(value string, numObstacles int) {
 		cellContainers[r] = make([]*fyne.Container, numCols)
 		for c := 0; c < numCols; c++ {
 			rect := canvas.NewRectangle(color.Black)
-			rect.SetMinSize(fyne.NewSize((40 - twiceObSize), (40 - twiceObSize)))
+			rect.SetMinSize(fyne.NewSize((40 - twiceObSize), (40 - twiceObSize))) // zoom in and out buttons will change this and refresh
 			cellContainer := container.NewStack(rect)
 			cellContainers[r][c] = cellContainer
 			grid.Add(cellContainer)
 		}
 	}
 
-	DeterminePath(cellContainers, numRows, numCols)
+	DeterminePath_setimages(TrackType, cellContainers, numRows, numCols)
 
 	cgrid := container.NewCenter(grid)
 	centeredGrid := container.New(layout.NewVBoxLayout(),
@@ -54,7 +54,7 @@ func Grid_Widget(value string, numObstacles int) {
 		HomeScreen()
 	})
 
-	gridWithHomeB := container.New(layout.NewBorderLayout(nil, homeButton, nil, nil),
+	gridWithHomeB := container.New(layout.NewBorderLayout(nil, homeButton, nil, nil), // create zoom in and out buttons, place in NewVBox, add to right side here
 		homeButton,
 		scrollingGrid,
 	)
@@ -62,29 +62,59 @@ func Grid_Widget(value string, numObstacles int) {
 	mainWin.SetContent(gridWithHomeB)
 }
 
-func SetImageInCell(CellGrid [][]*fyne.Container, row, col int, image *canvas.Image) {
-	image.FillMode = canvas.ImageFillContain
-	CellGrid[row][col].Objects = []fyne.CanvasObject{image}
-	CellGrid[row][col].Refresh()
+func SetImageInCell(CellGrid [][]*fyne.Container, row, col int, imageName_Path []string) {
+	if path, ok := ImagePaths[imageName_Path[0]]; ok {
+		path.FillMode = canvas.ImageFillContain
+		CellGrid[row][col].Objects = []fyne.CanvasObject{path}
+		CellGrid[row][col].Refresh()
+	} else {
+		image := canvas.NewImageFromFile(imageName_Path[1])
+		ImagePaths[imageName_Path[0]] = image
+		image.FillMode = canvas.ImageFillContain
+		CellGrid[row][col].Objects = []fyne.CanvasObject{image}
+		CellGrid[row][col].Refresh()
+	}
 }
 
-func DeterminePath(CellGrid [][]*fyne.Container, numRows, numCols int) {
-	R := numRows - 1
-	C := 0
+func DeterminePath_setimages(TrackType string, CellGrid [][]*fyne.Container, numRows, numCols int) {
+
+	var R int
+	var C int
+	if TrackType == "loop" {
+		R = numRows - 1
+		C = 0
+	}
+
+	if TrackType == "linear" {
+		R = numRows - 1
+		C = 1
+	}
 	var Trk []map[string]interface{}
 
 	start := map[string]interface{}{
 		"Position": []int{R, C},
+		"visited":  true,
 	}
 
 	Trk = append(Trk, start)
 
 	//how to add image
-	StartRightImage := canvas.NewImageFromFile("/home/mossblac/workspace/github.com/Mossblac/Rally_Mapper/images/startRight.png")
-	SetImageInCell(CellGrid, R, C, StartRightImage)
+
+	SetImageInCell(CellGrid, R, C, StartRight)
+	SetImageInCell(CellGrid, R, C+1, StartRight)
+	SetImageInCell(CellGrid, R-1, C+1, StartAngleUR)
+
+	start["image"] = "StartRightImage" // how to add image field
 
 	fmt.Printf("%+v\n", Trk)
 
-	//how to search your structs
+	/*if greater than 0, and less than numRows/Cols, and visited is false, add to list
+	then randomly pick from that list.
+
+	whichever is picked, create a new map named the picked coordinates AND set The "Position" value to the same.
+	in that new map set "previous" to the previous cell coordinates( the first will just be: R, C)
+
+	if previous == R+1, C-1 then previous image is set to face TopRightCorner and so on....
+	*/
 
 }
---
+++ MappedImages.go
@@ -0,0 +1,9 @@
+package main
+
+import "fyne.io/fyne/v2/canvas"
+
+var ImagePaths = make(map[string]*canvas.Image)
+
+var StartRight = []string{"StartRight", "/home/mossblac/workspace/github.com/Mossblac/Rally_Mapper/images/startRight.png"}
+var StartAngleUR = []string{"StartAngleUR", "/home/mossblac/workspace/github.com/Mossblac/Rally_Mapper/images/startAngleUR.png"}
+var StartUP = []string{"StartUp", "/home/mossblac/workspace/github.com/Mossblac/Rally_Mapper/images/startUp.png"}
