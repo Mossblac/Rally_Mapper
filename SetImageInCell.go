342c9d68625ef846bf35fc41077efcb23485110f
Mossblac <Murphyrmurphy2000@gmail.com>
2025-09-24 21:15:07 -0600

    added fade in animation

--- SetImageInCell.go
+++ SetImageInCell.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"image/color"
+	"time"
+
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/canvas"
 )
@@ -10,15 +13,29 @@ func SetImageInCell(CellGrid [][]*fyne.Container, row, col int, imageName_Path [
 		path.FillMode = canvas.ImageFillContain
 		CellGrid[row][col].Objects = []fyne.CanvasObject{path}
 		CellGrid[row][col].Refresh()
+		FadeInAnimate(path)
 	} else {
 		image := canvas.NewImageFromFile(imageName_Path[1])
 		ImagePaths[imageName_Path[0]] = image
 		image.FillMode = canvas.ImageFillContain
+		image.Translucency = 1.0
 		CellGrid[row][col].Objects = []fyne.CanvasObject{image}
 		CellGrid[row][col].Refresh()
+		FadeInAnimate(image)
 	}
 }
 
-func FadeInAnimate() {
+func FadeInAnimate(image *canvas.Image) {
+	fadeIn := canvas.NewColorRGBAAnimation(
+		color.RGBA{A: 0},
+		color.RGBA{A: 255},
+		time.Second*1,
+		func(c color.Color) {
+			_, _, _, a := c.RGBA()
+			image.Translucency = 1.0 - (float64(a) / 65535.0)
+			image.Refresh()
+		},
+	)
 
+	fadeIn.Start()
 }
