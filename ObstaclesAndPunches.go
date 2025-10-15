fcaf09b21d1567a4c7694e31854c175d60727ef9
Mossblac <Murphyrmurphy2000@gmail.com>
2025-10-15 14:16:10 -0600

    determine obstacles working

--- ObstaclesAndPunches.go
+++ ObstaclesAndPunches.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"fyne.io/fyne/v2"
+)
 
 func DetermineObstacles() {
 	for i := 0; i < len(SpacingList); i++ {
@@ -91,17 +95,100 @@ func NotPlacable(I int) bool {
 }
 
 func DetermineOBIconAndPlace(I int, obstacle string) { // ect, etc...
-	for i := range TFinish {
-		if Track[i].CurPosR == Track[I].CurPosR && Track[i].CurPosC == Track[I].CurPosC {
-			Track[i].Image.Ic4 = UnsetPlaceholdericon
-		}
-	}
-	Track[I].Image.Ic4 = UnsetPlaceholdericon
-	/*ic1 := Track[I].Image.Ic1
+	var ic4 *fyne.StaticResource
+	ic1 := Track[I].Image.Ic1
 	if obstacle == "LowBridge" {
 		switch ic1 {
-		case StraightDownicon, StraightUPicon:
-			Track[I].Image.Ic4 = LowBridge_Verticalicon
+		case StraightDownicon, StraightUPicon: //vertical
+			ic4 = LowBridge_Verticalicon
+		case StraightDBLicon, StraightDTRicon: //diag TR_BL
+			ic4 = LowBridge_D_TR_BLorBL_TRicon
+		case StraightDBRicon, StraightDTLicon: //diag BR_TL
+			ic4 = LowBridge_D_TL_BRorBR_TLicon
+		case StraightLefticon, StraightRighticon: // horizontal
+			ic4 = LowBridge_Horizontalicon
+		case CurveB_DTLicon, CurveDTL_Bicon: // curve B_TL
+			ic4 = LowBridge_Curve_B_TLorTL_Bicon
+		case CurveB_DTRicon, CurveDTR_Bicon: // curve B_TR
+			ic4 = LowBridge_Curve_B_TRorTR_Bicon
+		case CurveL_DBRicon, CurveDBR_Licon: // curve L_BR
+			ic4 = LowBridge_Curve_L_BRorBR_Licon
+		case CurveL_DTRicon, CurveDTR_Licon: //curve L_TR
+			ic4 = LowBridge_Curve_L_TRorTR_Licon
+		case CurveR_DBLicon, CurveDBL_Ricon: //curve R_BL
+			ic4 = LowBridge_Curve_R_BLorBL_Ricon
+		case CurveR_DTLicon, CurveDTL_Ricon: //curve R_TL
+			ic4 = LowBridge_Curve_R_TLorTL_Ricon
+		case CurveT_DBLicon, CurveDBL_Ticon: //curve T_BL
+			ic4 = LowBridge_Curve_T_BLorBL_Ticon
+		case CurveT_DBRicon, CurveDBR_Ticon: //curve T_BR
+			ic4 = LowBridge_Curve_T_BRorBR_Ticon
 		}
-	}*/
+	}
+
+	if obstacle == "ParkingBlock" {
+		switch ic1 {
+		case StraightDownicon, StraightUPicon: //vertical
+			ic4 = ParkingBlock_Verticalicon
+		case StraightDBLicon, StraightDTRicon: //diag TR_BL
+			ic4 = ParkingBlock_D_TR_BLorBL_TRicon
+		case StraightDBRicon, StraightDTLicon: //diag BR_TL
+			ic4 = ParkingBlock_D_TL_BRorBR_TLicon
+		case StraightLefticon, StraightRighticon: // horizontal
+			ic4 = ParkingBlock_Horizontalicon
+		case CurveB_DTLicon, CurveDTL_Bicon: // curve B_TL
+			ic4 = ParkingBlock_Curve_B_TLorTL_Bicon
+		case CurveB_DTRicon, CurveDTR_Bicon: // curve B_TR
+			ic4 = ParkingBlock_Curve_B_TRorTR_Bicon
+		case CurveL_DBRicon, CurveDBR_Licon: // curve L_BR
+			ic4 = ParkingBlock_Curve_L_BRorBR_Licon
+		case CurveL_DTRicon, CurveDTR_Licon: //curve L_TR
+			ic4 = ParkingBlock_Curve_L_TRorTR_Licon
+		case CurveR_DBLicon, CurveDBL_Ricon: //curve R_BL
+			ic4 = ParkingBlock_Curve_R_BLorBL_Ricon
+		case CurveR_DTLicon, CurveDTL_Ricon: //curve R_TL
+			ic4 = ParkingBlock_Curve_R_TLorTL_Ricon
+		case CurveT_DBLicon, CurveDBL_Ticon: //curve T_BL
+			ic4 = ParkingBlock_Curve_T_BLorBL_Ticon
+		case CurveT_DBRicon, CurveDBR_Ticon: //curve T_BR
+			ic4 = ParkingBlock_Curve_T_BRorBR_Ticon
+		}
+	}
+
+	if obstacle == "Curb_Drop" {
+		switch ic1 {
+		case StraightDownicon, StraightUPicon: //vertical
+			ic4 = CurbDrop_Verticalicon
+		case StraightDBLicon, StraightDTRicon: //diag TR_BL
+			ic4 = CurbDrop_D_TR_BLorBL_TRicon
+		case StraightDBRicon, StraightDTLicon: //diag BR_TL
+			ic4 = CurbDrop_D_TL_BRorBR_TLicon
+		case StraightLefticon, StraightRighticon: // horizontal
+			ic4 = CurbDrop_Horizontalicon
+		case CurveB_DTLicon, CurveDTL_Bicon: // curve B_TL
+			ic4 = CurbDrop_Curve_B_TLorTL_Bicon
+		case CurveB_DTRicon, CurveDTR_Bicon: // curve B_TR
+			ic4 = CurbDrop_Curve_B_TRorTR_Bicon
+		case CurveL_DBRicon, CurveDBR_Licon: // curve L_BR
+			ic4 = CurbDrop_Curve_L_BRorBR_Licon
+		case CurveL_DTRicon, CurveDTR_Licon: //curve L_TR
+			ic4 = CurbDrop_Curve_L_TRorTR_Licon
+		case CurveR_DBLicon, CurveDBL_Ricon: //curve R_BL
+			ic4 = CurbDrop_Curve_R_BLotBL_Ricon
+		case CurveR_DTLicon, CurveDTL_Ricon: //curve R_TL
+			ic4 = CurbDrop_Curve_R_TLotTL_Ricon
+		case CurveT_DBLicon, CurveDBL_Ticon: //curve T_BL
+			ic4 = CurbDrop_Curve_T_BLorBL_Ticon
+		case CurveT_DBRicon, CurveDBR_Ticon: //curve T_BR
+			ic4 = CurbDrop_Curve_T_BRorBR_Ticon
+		}
+	}
+
+	Track[I].Image.Ic4 = ic4
+
+	for i := range TFinish {
+		if Track[i].CurPosR == Track[I].CurPosR && Track[i].CurPosC == Track[I].CurPosC {
+			Track[i].Image.Ic4 = ic4
+		}
+	}
 }
