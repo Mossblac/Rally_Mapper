40a3b824914bfdbc1466ad429613ba32492e9a61
Mossblac <Murphyrmurphy2000@gmail.com>
2025-11-02 21:36:50 -0700

    created buttons and messages for confirming delete

--- LoadWindow.go
+++ LoadWindow.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"image/color"
 
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/canvas"
@@ -9,20 +10,50 @@ import (
 	"fyne.io/fyne/v2/widget"
 )
 
-func LoadT() {
+var Delete_Activator bool
 
-	//del button
-	//del mode window. hide
-	//confirm window. hide to pass into DelT
+func LoadT() {
+	redLevelselect := &color.RGBA{R: 255, G: 0, B: 0, A: 25}
+	redLevelConfirm := &color.RGBA{R: 255, G: 0, B: 0, A: 75}
+	Del_Sel_rec := canvas.NewRectangle(redLevelselect)
+	Del_Sel_rec.Hide()
+
+	Del_Confirm_red := canvas.NewRectangle(redLevelConfirm)
+	Del_Confirm_red.Hide()
 
 	Home := widget.NewButton("home", func() {
 		HomeScreen()
 	})
-	CHome := container.NewCenter(Home)
 
 	text := widget.NewLabel("Select Track to Load")
 	text.Alignment = fyne.TextAlignCenter
 	Ctext := container.NewCenter(text)
+
+	DeleteBut := widget.NewButton("delete", func() {
+		Delete_Activator = true
+		text.Text = "Select Track to Delete"
+		text.Refresh()
+		Del_Sel_rec.Show()
+	})
+
+	Comfirm_text := widget.NewLabel("Confirm Delete Selected Track")
+	text.Alignment = fyne.TextAlignCenter
+	Cent_Confirm_text := container.NewCenter(Comfirm_text)
+
+	Confirmed_DeleteBut := widget.NewButton("DELETE", func() {
+
+	})
+	CancelDeleteBut := widget.NewButton("Cancel", func() {
+
+	})
+
+	ConfirmButtons := container.NewHBox(CancelDeleteBut, Confirmed_DeleteBut)
+	ConfirmBox := container.NewVBox(Cent_Confirm_text, ConfirmButtons)
+	CentConfirmBox := container.NewCenter(ConfirmBox)
+	CentConfirmBox.Hide()
+
+	main_buttons := container.NewHBox(Home, DeleteBut)
+	Cbuttons := container.NewCenter(main_buttons)
 
 	cat, err := LoadCatalog("./Saves/catalog.json")
 	if err != nil {
@@ -33,39 +64,34 @@ func LoadT() {
 	resBK := fyne.NewStaticResource("images/menuBkG.jpg", resourceMenuBkGJpgData)
 	bkg := canvas.NewImageFromResource(resBK)
 
-	centerStack := container.NewStack(bkg, catList)
+	centerStack := container.NewStack(bkg, Del_Sel_rec, Del_Confirm_red, catList, CentConfirmBox)
 
-	LoadListWithButton := container.NewBorder(Ctext, CHome, nil, nil, centerStack)
+	LoadListWithButton := container.NewBorder(Ctext, Cbuttons, nil, nil, centerStack)
 
 	mainWin.SetContent(LoadListWithButton)
 }
 
 func onSelect(T CatalogEntry) {
-	Loading = true
-	TrkPoint, err := LoadTrack("./Saves", T.ID)
-	if err != nil {
-		fmt.Printf("error loading Track: %v", err)
-	}
-	var trackT string
-	if T.Ttype {
-		trackT = "loop"
+	if Delete_Activator {
+		//need to find a way to have this show Del_Confirm_red and CentConfirmBox
 	} else {
-		trackT = "linear"
+		Loading = true
+		TrkPoint, err := LoadTrack("./Saves", T.ID)
+		if err != nil {
+			fmt.Printf("error loading Track: %v", err)
+		}
+		var trackT string
+		if T.Ttype {
+			trackT = "loop"
+		} else {
+			trackT = "linear"
+		}
+		Trk := *TrkPoint
+		Grid_Widget(trackT, Trk.TSize, Trk)
 	}
-	Trk := *TrkPoint
-	Grid_Widget(trackT, Trk.TSize, Trk)
-
 }
 
-func DelT() {
-	// delete mode window (red square, message change).hide within LoadT, shows upon button press.
-	//del confirm window, also hidden, pass into DelT to show
-
-	//LoadT window with red partially transparent square on top of background
-	//message changes to "select track to delete".
-
-	//when selected, show the confirmation screen, "confirm delete Track : %v"
-	//confirm screen has less transparent red background and small centered hbox with confirm and cancel buttons.
-
-	//confirm will delete entry and return to LoadT, cancel will return to LoadT without deleting.
+func DelConfirm(clBk *canvas.Rectangle, buttons *fyne.Container) {
+	clBk.Show()
+	buttons.Show()
 }
